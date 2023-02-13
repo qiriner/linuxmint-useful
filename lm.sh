68931633ba38d0c534989357128bd2fc69a92deb
qiriner <4958072+qiriner@users.noreply.github.com>
2023-02-13 17:37:01 +0300

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -1,10 +1,9 @@
-###Tested in Linux Mint 21.1
+#!/bin/sh
+#Tested in Linux Mint 21.1
 
-###LibreOffice (fresh version). Will natively update
+##LibreOffice repository (current stable version)
 sudo add-apt-repository ppa:libreoffice/ppa
 
-###JOSM repo
-# Download the key
+##JOSM repository
 wget -q https://josm.openstreetmap.de/josm-apt.key -O- | sudo gpg --dearmor -o /etc/apt/keyrings/josm-apt.gpg
-# Add repository
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/josm-apt.gpg] https://josm.openstreetmap.de/apt alldist universe" | sudo tee /etc/apt/sources.list.d/josm.list > /dev/null
