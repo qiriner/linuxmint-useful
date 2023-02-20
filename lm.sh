ae9339833cfdc875ce5c1521006a7fb01c6c46cb
qiriner <4958072+qiriner@users.noreply.github.com>
2023-02-20 13:53:11 +0300

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -7,3 +7,6 @@ sudo add-apt-repository ppa:libreoffice/libreoffice-still
 ##JOSM repository
 wget -q https://josm.openstreetmap.de/josm-apt.key -O- | sudo gpg --dearmor -o /etc/apt/keyrings/josm-apt.gpg
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/josm-apt.gpg] https://josm.openstreetmap.de/apt alldist universe" | sudo tee /etc/apt/sources.list.d/josm.list > /dev/null
+
+##Microsoft fonts#
+sudo apt install ttf-mscorefonts-installer
