e94be492af1820237f2f1f5d399ab8659b2aed88
qiriner <4958072+qiriner@users.noreply.github.com>
2023-04-07 15:00:32 +0300

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 #Tested in Linux Mint 21.1
 
-##LibreOffice repository (stable version)
-sudo add-apt-repository ppa:libreoffice/libreoffice-still
+##LibreOffice repository
+sudo add-apt-repository ppa:libreoffice/ppa
 
-##JOSM repository
-wget -q https://josm.openstreetmap.de/josm-apt.key -O- | sudo gpg --dearmor -o /etc/apt/keyrings/josm-apt.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/josm-apt.gpg] https://josm.openstreetmap.de/apt alldist universe" | sudo tee /etc/apt/sources.list.d/josm.list > /dev/null
+##JOSM repository & install
+wget -P /etc/apt/keyrings https://josm.openstreetmap.de/josm-apt.key
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/josm-apt.key] https://josm.openstreetmap.de/apt alldist universe" | sudo tee /etc/apt/sources.list.d/josm.list > /dev/null
+sudo apt install josm
 
-##Microsoft fonts#
+##Microsoft fonts install
 sudo apt install ttf-mscorefonts-installer
+
+##Wine repository & install
+sudo dpkg --add-architecture i386
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+sudo apt install --install-recommends winehq-stable
