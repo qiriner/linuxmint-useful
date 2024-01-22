24d103892b481aee1f2747f0c376bc87197f2eed
qiriner <4958072+qiriner@users.noreply.github.com>
2024-01-22 10:04:32 +0300

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-#Tested in Linux Mint 21.1
+#For Linux Mint 21.x
 
-#LibreOffice repository
+#LibreOffice
+##Backport
+sudo apt install -t jammy-backports libreoffice
+##Repository
 sudo add-apt-repository ppa:libreoffice/ppa
 
-#GIMP repository (UNofficial)
+#Microsoft fonts install
+sudo apt install ttf-mscorefonts-installer
+
+#GIMP repository (Unofficial)
 sudo add-apt-repository ppa:ubuntuhandbook1/gimp
 
 #JOSM repository & install
 sudo wget -P /etc/apt/keyrings https://josm.openstreetmap.de/josm-apt.key
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/josm-apt.key] https://josm.openstreetmap.de/apt alldist universe" | sudo tee /etc/apt/sources.list.d/josm.list > /dev/null
 sudo apt install josm
-
-#Microsoft fonts install
-sudo apt install ttf-mscorefonts-installer
 
 #Wine repository & install
 sudo dpkg --add-architecture i386
