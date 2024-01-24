dbffa8856703fad10ca45c35419e01241b92d3f0
qiriner <4958072+qiriner@users.noreply.github.com>
2024-01-24 10:03:53 +0300

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -4,22 +4,25 @@
 #LibreOffice
 ##Backport
 sudo apt install -t jammy-backports libreoffice
-##Repository
+##PPA repository
 sudo add-apt-repository ppa:libreoffice/ppa
 
-#Microsoft fonts install
+#Microsoft Core Fonts
 sudo apt install ttf-mscorefonts-installer
 
-#GIMP repository (Unofficial)
+#GIMP PPA unofficial repository
 sudo add-apt-repository ppa:ubuntuhandbook1/gimp
+
+#Wine
+sudo apt install wine-installer
+
+#VirtualBox Guest Additions
+sudo apt install virtualbox-guest-x11
+
+#Open VM Tools for VMWare
+sudo apt install open-vm-tools-desktop
 
 #JOSM repository & install
 sudo wget -P /etc/apt/keyrings https://josm.openstreetmap.de/josm-apt.key
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/josm-apt.key] https://josm.openstreetmap.de/apt alldist universe" | sudo tee /etc/apt/sources.list.d/josm.list > /dev/null
 sudo apt install josm
-
-#Wine repository & install
-sudo dpkg --add-architecture i386
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
-sudo apt install --install-recommends winehq-stable
