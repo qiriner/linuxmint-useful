319605f644dff83767fbed29ee3d175a034975ee
qiriner <4958072+qiriner@users.noreply.github.com>
2024-03-09 12:04:29 +0300

    Update lm.sh

--- lm.sh
+++ lm.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-#For Linux Mint 21.x
+#For Linux Mint 21.x, based on Ubuntu 22.04.x
 
 #LibreOffice
 ##Backport
@@ -10,7 +9,10 @@ sudo add-apt-repository ppa:libreoffice/ppa
 #Microsoft Core Fonts
 sudo apt install ttf-mscorefonts-installer
 
-#GIMP PPA unofficial repository
+#GIMP
+##Basic repository
+sudo apt install gimp
+##PPA unofficial repository
 sudo add-apt-repository ppa:ubuntuhandbook1/gimp
 
 #Wine
@@ -21,6 +23,10 @@ sudo apt install virtualbox-guest-x11
 
 #Open VM Tools for VMWare
 sudo apt install open-vm-tools-desktop
+
+#Fix keyboard layout switch (aka Bug #865)
+sudo add-apt-repository ppa:nrbrtx/xorg-hotkeys
+sudo apt dist-upgrade
 
 #JOSM repository & install
 sudo wget -P /etc/apt/keyrings https://josm.openstreetmap.de/josm-apt.key
